8c4e670ee3f8835ae6dea0b17a8acec9ef2c0e39
HECTOR JOSUE LOPEZ NUÑEZ <hectorjosueoficial@gmail.com>
2025-12-04 12:45:49 -0600

    actualizacion de dashboard prueba

--- public/js/RouterAPI/Autenticacion/Index.js
+++ public/js/RouterAPI/Autenticacion/Index.js
@@ -41,32 +41,7 @@ console.log("dashboard.EquiposEnTransito",dashboard.EquiposEnTransito);
     });*/
     // Verifica con la API solo si quieres "refrescar" o validar (no en cada carga)
     //  solo en la página de inicio o si el token podría expirar pronto
-    const shouldVerify = window.location.pathname === '/inicio';  // O tu lógica
-    if (shouldVerify) {
-        const urlProtected = `${api}/api/logintecnicos/protected`;
-        try {
-            const response = await fetch(urlProtected, {
-                method: 'GET',
-                credentials: 'include',
-            });
-            if (response.ok) {
-                const data = await response.json();
-                // Actualiza localStorage y UI con datos frescos
-                localStorage.setItem('username', data.data.usuario);
-                titulo.textContent = data.data.usuario;
-                nombreperfil.textContent = data.data.usuario;
-           
-            } else if (response.status === 401) {
-                // Limpia localStorage y redirige
-                localStorage.removeItem('username');
-                window.location.href = '/logintecnico';
-            }
-        } catch (error) {
-            console.error('Error al verificar sesión:', error);
-            // No rediriges aquí para no interrumpir la UX; solo loguea
-        }
-    }
-
+    
 
     btnlogout.addEventListener('click', async function (event) {
         event.preventDefault();
@@ -99,40 +74,5 @@ console.log("dashboard.EquiposEnTransito",dashboard.EquiposEnTransito);
     });
 });
 
-// Función para obtener datos con cache local
-async function fetchComponentes() {
-    
-    const cached = localStorage.getItem(cacheKey);
-    const cacheTime = localStorage.getItem(cacheKey + '_time');
-    const now = Date.now();
 
-    // Si hay cache y no ha expirado (ej. 1 hora)
-    if (cached && cacheTime && (now - cacheTime) < 3600000) {
-        console.log('Datos desde localStorage');
-        return JSON.parse(cached);
-    }
-    const url = `${api}/api/reportes/reporte/ConsultaTotalRetirosEnTransito`;
-    try {
-        const response = await fetch(url, {
-            method: 'GET',
-            //credentials: 'include',
-        });
-      
- console.log("response", data);
-       /* if (data) {
-             //const data = await response.json();
-            // Guarda en localStorage
-            //localStorage.setItem(cacheKey, JSON.stringify(data));
-            //localStorage.setItem(cacheKey + '_time', now.toString());
-
-            //return data;
-            
-        }*/
-    } catch (error) {
-        console.error('Error de conexión:', error);
-    }
-
-
-
-}
 
